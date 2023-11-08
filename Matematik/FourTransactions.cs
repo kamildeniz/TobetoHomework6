a3e9990f75d94c53fbdc37ac66093149a8cce9e2
kamil07deniz <kamil07deniz@gmail.com>
2023-11-08 19:37:37 +0300

    yazılımcı Geliştirici kampı 3. video bitti

---
+++ Matematik/FourTransactions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematik
+{
+    public class FourTransactions
+    {
+        public void Gather(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+        public void Multiplication(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum *= numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Matematik;
+
+FourTransactions transactions = new FourTransactions();
+transactions.Gather(5, 8, 9, 6, 65, 56);
